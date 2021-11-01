4d429bf11190d9c0301b4a44072ac82122857d02
Fatema Moharam <89610248+moharamfatema@users.noreply.github.com>
2021-11-02 01:09:54 +0200

    neted objects

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,36 +1,68 @@
-import React from "react";
-import {Button, Card, Container, Form, FormGroup, Row,Stack} from "react-bootstrap";
-import './Signup.css'
-
+import {useState} from "react";
+import {Card, Col, Container, Form, Image, Stack, Row, Button} from "react-bootstrap";
+import {render} from "react-dom";
+import errorImage from '../images/icon-error.svg'
+import './Signup.scss'
 
 export default function Signup(){
+    const [user,setUser] = useState({firstName:{value:"",error:false,errmsg:""},
+        LastName:{value:"",error:false,errmsg:""},
+        email:{value:"",error:false,errmsg:""},
+        password:{value:"",error:false,errmsg:""}
+    })
+    const getString = (key)=>{
+        switch (key){
+            case 0:
+                return 'First Name';
+            case 1:
+                return 'Last Name';
+            case 2:
+                return 'E-mail Address';
+            case 3:
+                return 'Password';
+        }
+    }
 
+    const onChange = (e) =>{
+        setUser(prevUser=>({...prevUser,[e.target.id]:{...prevUser[e.target.id],prevUser[e.target.id].value:e.target.value};
+    }
+    const onSubmit = (e) =>{
 
-        return(
-            <Card>
-                <Card.Body>
+    }
+    return(
+        <Card>
+            <Card.Body>
+                <Form onSubmit={onSubmit}>
+                    <Stack gap={3}>
+                        {Object.keys(user).map((userInfo,i) =>(
+                                <Form.Group className={"formGroup"} id={`${userInfo}`}>
+                                    <Container >
+                                        <span>
+                                            <Form.Control
+                                                type={(i == 3) ? 'password' : (i == 2) ? 'email' : 'text'}
+                                                placeholder={getString(i)}
+                                                className={(user[userInfo].error)?"formControl error":"formControl"}
+                                                onChange={onChange}
+                                                id={userInfo}
+                                            />
+                                            {(user[userInfo].error) ?
+                                                (<Image src={errorImage} className='errorImage'/>) :
+                                                (<></>)
+                                            }
+                                            {(user[userInfo].error) ?
+                                                (<Form.Label className={"error"}>{user[userInfo].errmsg}</Form.Label>) :
+                                                (<></>)
+                                            }
 
-                        <Form >
-                            <Stack gap={3}>
-                            <Form.Group as={Row} >
-                                <Form.Control className="formControl" type="input" placeholder="First Name"></Form.Control>
-                            </Form.Group >
-                            <Form.Group as={Row}>
-                                <Form.Control className="formControl" type="input" placeholder="Last Name"></Form.Control>
-                            </Form.Group>
-                            <Form.Group as={Row}>
-                                <Form.Control className="formControl" type="email" placeholder="Email Address"></Form.Control>
-                            </Form.Group>
-                            <Form.Group as={Row}>
-                                <Form.Control className="formControl" type="password" placeholder="Password"></Form.Control>
-                            </Form.Group>
-                            <Button  as={Row} className="formControl button" type="submit" id={"submit-button"}>CLAIM YOUR FREE TRIAL</Button>
-                            </Stack>
-                        </Form>
-
-                </Card.Body>
-            </Card>
-        )
-
-
+                                        </span>
+                                    </Container>
+                                </Form.Group>
+                        ))
+                        }
+                    </Stack>
+                    <Button type="submit" onClick={onSubmit}>CLAIM YOUR FREE TRIAL</Button>
+                </Form>
+            </Card.Body>
+        </Card>
+    )
 }
