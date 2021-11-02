a55a1772250287ffecddf45df1fb818df28a0ddf
Fatema Moharam <89610248+moharamfatema@users.noreply.github.com>
2021-11-02 21:38:07 +0200

    developing step:done - styling: done

--- src/App.js
+++ src/App.js
@@ -13,14 +13,7 @@ function App() {
                       <h1>Learn to code by watching others</h1>
                       <p>See how experienced developers solve problems in real-time.
                           Watching scripted
-                          tutorials is great, but understanding how developers think is invaluable. Try it free 7 days
-                          then
-                          $20/mo.
-                          thereafter
-                          First Name Last Name Email Address Password Claim your free trial By clicking the button, you
-                          are
-                          agreeing to our
-                          Terms and Services</p>
+                          tutorials is great, but understanding how developers think is invaluable.</p>
                   </div>
               </Col>
               <Col>
